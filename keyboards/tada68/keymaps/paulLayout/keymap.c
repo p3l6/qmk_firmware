a5fdcd6df8c69495947f4cc502b8d349930cee98
Paul Landers <paul.landers@testplant.com>
2017-12-15 11:48:17 -0700

    Changes to tada layout

--- keyboards/tada68/keymaps/paulLayout/keymap.c
+++ keyboards/tada68/keymaps/paulLayout/keymap.c
@@ -7,6 +7,16 @@
 #define _BL 0
 #define _FL 1
 
+enum custom_keycodes {
+    Z_NSSTR = SAFE_RANGE,
+    Z_NSLOG,
+    Z_ATQOT,
+    Z_ARROW,
+    Z_STCMT,
+    Z_ENCMT,
+    Z_SFRMT,
+};
+
 #define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -32,62 +42,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
-   * | ~ | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|       |    |
+   * |Esc| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|       |    |
    * |----------------------------------------------------------------|
-   * | Bksp|bdn|bup|exp| c~|Bl-|BL |BL+|   |   |   | M | M |     |    |
+   * | Bksp|bdn|bup|exp|  |Bl-|BL |BL+|   |   |   | M | M |  M   |    |
    * |----------------------------------------------------------------|
-   * |  Fn  |mut|vdn|vup|ct |   |   |   |   |   |   | M |Num Ret |Hme |
+   * |  Fn  |mut|vdn|vup|   |   |   |   |   |  M|   | M |Num Ret |Hme |
    * |----------------------------------------------------------------|
    * |  CAPS  |prv|ply|nxt|   |   | M |   |   | M |   |      |   |End |
    * |----------------------------------------------------------------|
-   * |    |    |    |            Enter     |   | Fn | F16|F17|F18|f19 |
+   * |    |    |    |            Enter     |Dup| Fn | F16|F17|F18|f19 |
    * `----------------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
-  KC_TILD, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______ ,  \
-  KC_BSPC,KC_SLCK, KC_PAUS, LCTL(KC_UP), LGUI(KC_TILD),BL_DEC,BL_TOGG,BL_INC,_______,_______, _______,    M(0),   M(1),_______,_______, \
-  _______,KC_MUTE, KC_VOLD, KC_VOLU, LGUI(KC_TAB),_______,_______,_______,_______,_______,_______, M(2),    KC_PENT, KC_HOME, \
-  KC_CAPS,KC_MRWD, KC_MPLY, KC_MFFD, _______,_______, M(4),_______,_______,M(3),_______,_______, _______, KC_END, \
-  _______,_______,_______,                 KC_ENT,               _______,_______,KC_F16,KC_F17,KC_F18, KC_F19),
+  KC_ESC, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______ ,  \
+  KC_BSPC,KC_SLCK, KC_PAUS, LCTL(KC_UP), _______,BL_DEC,BL_TOGG,BL_INC,_______,_______, _______,    Z_STCMT,Z_ENCMT,Z_SFRMT,_______, \
+  _______,KC_MUTE, KC_VOLD, KC_VOLU, _______,_______,_______,_______,_______,Z_NSLOG,_______, Z_ATQOT,    KC_PENT, KC_HOME, \
+  KC_CAPS,KC_MRWD, KC_MPLY, KC_MFFD, _______,_______,Z_NSSTR,_______,_______,Z_ARROW,_______,_______, _______, KC_END, \
+  _______,_______,_______,                 KC_ENT,          LCTL(LALT(KC_DOWN)),_______,KC_F16,KC_F17,KC_F18, KC_F19),
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0: // block comment  / *
-          if (record->event.pressed) {
-          } else {
-            return MACRO( T(SLSH), D(LSFT), T(8), U(LSFT),  END);
-          }
-          break;
-
-        case 1: // block comment  * /
-          if (record->event.pressed) {
-          } else {
-            return MACRO(  D(LSFT), T(8), U(LSFT), T(SLSH),  END);
-          }
-          break;
-
-        case 2: // @"%@"
-          if (record->event.pressed) {
-          } else {
-            return MACRO( D(LSFT), T(2), T(QUOT), T(5), T(2), T(QUOT), U(LSFT),  END);
-          }
-          break;
-
-        case 3: // ->
-          if (record->event.pressed) {
-          } else {
-            return MACRO( T(MINS), D(LSFT), T(DOT), U(LSFT),  END);
-          }
-          break;
-	case 4: // NSString*
-          if (record->event.pressed) {
-          } else {
-            return MACRO( D(LSFT), T(N), T(S), T(S), U(LSFT), T(T),T(R),T(I),T(N),T(G), D(LSFT), T(8), U(LSFT),  END);
-          }
-          break;
-      }
-    return MACRO_NONE;
-};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // See https://docs.qmk.fm/macros.html
+  if (record->event.pressed) {
+    switch(keycode) {
+      case Z_NSSTR:
+        SEND_STRING("NSString*");
+        return false; break;
+      case Z_NSLOG:
+        SEND_STRING("NSLog(@\"\");"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT));
+        return false; break;
+      case Z_ATQOT:
+        SEND_STRING("@\"%%@\",");
+        return false; break;
+      case Z_ARROW:
+        SEND_STRING("->");
+        return false; break;
+      case Z_STCMT:
+        SEND_STRING("/*");
+        return false; break;
+      case Z_ENCMT:
+        SEND_STRING("*/");
+        return false; break;
+      case Z_SFRMT:
+        SEND_STRING("\\()"SS_TAP(X_LEFT));
+        return false; break;
+    }
+  }
+  return true;
+}

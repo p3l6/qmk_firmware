1e14cffaccae93a0358441f6b2ae8e2c7a73466b
Paul Landers <planders@statestitle.com>
2020-02-25 15:54:43 -0500

    Add continue key to left arrow

--- keyboards/playkbtw/ca66/keymaps/pwxn/keymap.c
+++ keyboards/playkbtw/ca66/keymaps/pwxn/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_MOD,          RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______, _______,  _______, _______,          _______,
     _______,          _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PENT, _______, _______,           _______,          _______,
     _______, XXXXXXX, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______,           KC_F19,           KC_F16,
-    XXXXXXX,          _______, _______,          XXXXXXX,          KC_P0,            XXXXXXX,          KC_PDOT, _______,  _______, KC_F18,  KC_F17),
+    XXXXXXX,          _______, _______,          XXXXXXX,          KC_P0,            XXXXXXX,          KC_PDOT, _______,  KC_F16,  KC_F18,  KC_F17),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

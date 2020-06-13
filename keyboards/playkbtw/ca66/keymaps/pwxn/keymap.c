fed3db2d3371dc84454c52ffeb0bb10025d3c68a
Paul Landers <planders@statestitle.com>
2020-06-13 10:23:28 -0400

    Split backspace on ca66

--- keyboards/playkbtw/ca66/keymaps/pwxn/keymap.c
+++ keyboards/playkbtw/ca66/keymaps/pwxn/keymap.c
@@ -14,24 +14,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
 
 [_BL] = LAYOUT(
-    KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, XXXXXXX, KC_DEL,
-    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,          KC_DEL,
-    MO(_NL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,           LT(_ML,KC_PGUP),
-    KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,           KC_UP,            KC_PGDN,
+    KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_DEL,  KC_GRV,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,          KC_PGUP,
+    MO(_NL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,           KC_PGDN,
+    KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,           KC_UP,            MO(_ML),
     XXXXXXX,          KC_LALT, KC_LGUI,          XXXXXXX,          KC_SPC,           XXXXXXX,          KC_RGUI, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT),
 
 [_NL] = LAYOUT(
-    KC_ESC,           __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,  __xxx__, __xxx__, XXXXXXX, KC_GRV,
+    KC_ESC,           __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__,  __xxx__, __xxx__, __xxx__, __xxx__,
     KC_LCTL,          __xxx__, KC_UP,   D_EXPOS, __xxx__, D_SPC_L, D_SPC_R, __xxx__, KC_UP,   __xxx__, KC_PGUP, KC_HOME,  __xxx__, KC_DEL,           __xxx__,
     _______,          KC_LEFT, KC_DOWN, KC_RGHT, __xxx__, D_TAB_L, D_TAB_R, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_END,            KC_PENT,          __xxx__,
     _______, XXXXXXX, D_DBG_C, D_DBG_N, D_DBG_S, D_DBG_O, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, _______,           __xxx__,          __xxx__,
     XXXXXXX,          _______, _______,          XXXXXXX,          _______,          XXXXXXX,          _______, KC_NO,    __xxx__, __xxx__, __xxx__),
 
 [_ML] = LAYOUT(
-    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_F13,  XXXXXXX, KC_GRV,
+    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_F13,  __xxx__, __xxx__,
     __xxx__,          KC_SLCK, KC_PAUS, BL_DEC,  BL_INC,  BL_TOGG, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, Z_STCMT,  Z_ENCMT, KC_DEL,           __xxx__,
-    __xxx__,          KC_MUTE, KC_VOLD, KC_VOLU, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, Z_JSSTR,           KC_PENT,          _______,
-    KC_CAPS, XXXXXXX, KC_MRWD, KC_MPLY, KC_MFFD, __xxx__, __xxx__, __xxx__, __xxx__, Z_ARROW, Z_LGARW, __xxx__, _______,           _______,          __xxx__,
+    __xxx__,          KC_MUTE, KC_VOLD, KC_VOLU, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, __xxx__, Z_JSSTR,           KC_PENT,          __xxx__,
+    KC_CAPS, XXXXXXX, KC_MRWD, KC_MPLY, KC_MFFD, __xxx__, __xxx__, __xxx__, __xxx__, Z_ARROW, Z_LGARW, __xxx__, _______,           _______,          _______,
     XXXXXXX,          __xxx__, __xxx__,          XXXXXXX,          __xxx__,            XXXXXXX,        __xxx__, __xxx__, _______,  _______,  _______),
 };
 
